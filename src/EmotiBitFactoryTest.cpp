fcf9d5eb0c454d95be287656a20ba32c9ff681f9
Nitin Nair <nitin7nair@gmail.com>
2021-08-04 07:08:58 -0700

    Moved testing typetags to new file. Added function to handle serial output update

---
+++ src/EmotiBitFactoryTest.cpp
@@ -0,0 +1,42 @@
+#include "EmotiBitFactoryTest.h"
+
+// Factory Test Typetags
+const char* EmotiBitFactoryTest::TypeTag::EMOTIBIT_VERSION = "EV\0";
+const char* EmotiBitFactoryTest::TypeTag::FIRMWARE_VERSION = "FV\0";
+const char* EmotiBitFactoryTest::TypeTag::I2C_COMM_INIT = "TW";
+const char* EmotiBitFactoryTest::TypeTag::FLASH = "FL\0";
+const char* EmotiBitFactoryTest::TypeTag::SERIAL_NUMBER_WRITE = "SW\0";
+const char* EmotiBitFactoryTest::TypeTag::LED_CONTROLLER = "LC\0";
+const char* EmotiBitFactoryTest::TypeTag::PPG_SENSOR = "PG\0";
+const char* EmotiBitFactoryTest::TypeTag::IMU_ID = "II\0";
+const char* EmotiBitFactoryTest::TypeTag::ACCEL_GYRO = "AG\0";
+const char* EmotiBitFactoryTest::TypeTag::MAGNETOMETER = "MG\0";
+const char* EmotiBitFactoryTest::TypeTag::TEMP_SENSOR = "TE\0";
+const char* EmotiBitFactoryTest::TypeTag::THERMOPILE = "TI\0";
+const char* EmotiBitFactoryTest::TypeTag::ADC_INIT = "AD\0";
+const char* EmotiBitFactoryTest::TypeTag::SD_CARD = "SD\0";
+const char* EmotiBitFactoryTest::TypeTag::WIFI = "WF\0";
+const char* EmotiBitFactoryTest::TypeTag::SETUP_COMPLETE = "SC\0";
+const char* EmotiBitFactoryTest::TypeTag::AVDD_VOLTAGE = "AV\0";
+const char* EmotiBitFactoryTest::TypeTag::DVDD_VOLTAGE = "DV\0";
+const char* EmotiBitFactoryTest::TypeTag::TOTAL_CURRENT = "TC\0";
+const char* EmotiBitFactoryTest::TypeTag::WIFI_OFF_CURRENT = "WC\0";
+const char* EmotiBitFactoryTest::TypeTag::HIBERNATE_CURRENT = "HC\0";
+const char* EmotiBitFactoryTest::TypeTag::TEST_RESULT_DELIMITER = ":\0";
+const char* EmotiBitFactoryTest::TypeTag::TEST_TYPE_DELIMITER = "+\0";
+const char* EmotiBitFactoryTest::TypeTag::TEST_PASS = "PASS\0";
+const char* EmotiBitFactoryTest::TypeTag::TEST_FAIL = "FAIL\0";
+const char* EmotiBitFactoryTest::TypeTag::NULL_VAL = "\0";
+
+void EmotiBitFactoryTest::updateOutputString(char* output, const char* testType, const char* result)
+{
+	//char output[10]; // max posible for any test
+	strcat(output,testType);
+	if(testType != TypeTag::SETUP_COMPLETE)
+	{
+		strcat(output,TypeTag::TEST_RESULT_DELIMITER);
+		strcat(output,result);
+		strcat(output,TypeTag::TEST_TYPE_DELIMITER);
+	}
+}
+
